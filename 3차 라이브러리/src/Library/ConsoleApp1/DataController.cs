224a374ae2379057e1803086a535fa9c8d106157
10Hyuna <kimha105@naver.com>
2023-04-08 20:18:46 +0900

    [ADD] MemberData, BookData DTO, VO 일부 구현

---
+++ 3차 라이브러리/src/Library/ConsoleApp1/DataController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DataController
+    {
+
+    }
+
+    class BookData
+    {
+
+        public List<string> title { get; set; }
+        public List<string> author { get; set; }
+        public List<string> publisher { get; set; }
+        public List<int> amount { get; set; }
+        public List<int> price { get; set; }
+        public List<string> publishDay { get; set; }
+        public List<int> ISBN { get; set; }
+        public List<string> information { get; set; }
+    }
+
+    class MemberData
+    {
+        public List<string> id { get; set;}
+        public List<string> password { get; set;}
+        public List<string> name { get; set;}
+        public List<int> age { get; set;}
+        public List<string> phoneNumber { get; set;}
+        public List<string> address { get; set;}
+
+    }
+
+    class UserData : MemberData
+    {
+
+    }
+
+    class ManagerData : MemberData
+    {
+
+    }
+}

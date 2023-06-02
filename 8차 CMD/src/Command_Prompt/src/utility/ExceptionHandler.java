9b07ad92f67c0b12780c5225002235c5a2d32b43
10Hyuna <kimha105@naver.com>
2023-06-02 10:29:28 +0900

    [delete] 안 쓰는 함수, 클래스

--- 8차 CMD/src/Command_Prompt/src/utility/ExceptionHandler.java
+++ 8차 CMD/src/Command_Prompt/src/utility/ExceptionHandler.java
@@ -4,18 +4,6 @@ import view.PrinterMessage;
 
 public class ExceptionHandler
 {
-    public boolean isExistencePath(String path)
-    {
-        return true;
-    }
-    public boolean isExistenceFile(String file)
-    {
-        return true;
-    }
-    public boolean isExistenceDirectory(String directory)
-    {
-        return true;
-    }
     public boolean isValidCommand(String command)
     {
         switch (command)

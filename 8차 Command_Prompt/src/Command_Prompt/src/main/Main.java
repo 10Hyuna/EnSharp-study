7e1d2087a85bf244af540be56bf8d3fe2c4ef020
10Hyuna <kimha105@naver.com>
2023-06-05 11:12:14 +0900

    [delete] 드라이브 볼륨 일련 번호 찾던 로직

--- 8차 Command_Prompt/src/Command_Prompt/src/main/Main.java
+++ 8차 Command_Prompt/src/Command_Prompt/src/main/Main.java
@@ -2,39 +2,11 @@ package main;
 
 import controller.InputCommand;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.FileStore;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String command = "wmic volume get serialnumber";
-        String serialNumber = getCommandOutput(command);
-        System.out.println("Volume Serial Number: " + serialNumber);
-//        InputCommand inputCommand = new InputCommand();
-//        inputCommand.inputCommand();
-    }
-    private static String getCommandOutput(String command) {
-        StringBuilder output = new StringBuilder();
-
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line.trim());
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return output.toString();
+        InputCommand inputCommand = new InputCommand();
+        inputCommand.inputCommand();
     }
 }
